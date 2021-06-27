032705708a4a9d8d7ab5f07f663531f71557ec1c
alex <60008473+alex83d@users.noreply.github.com>
2021-06-27 23:56:59 +0300

    team js

--- src/team.js
+++ src/team.js
@@ -6,31 +6,36 @@ for (let index = 0; index < buttons.length; index++) {
     const element = buttons[index];
     console.log(element)
     element.addEventListener('click', ev => {
-        ev.preventDefault(); 
-         //open
-         const content = ev.target.nextElementSibling;
-         console.log(content);
-         //content.style.maxHeight = "100vh";
-         //content.style.overflow = "visible";
+        ev.preventDefault();
+        //open
+        const content = ev.target.nextElementSibling;
+        /*
+        //content.style.maxHeight = "100vh";
+        //content.style.overflow = "visible";
+        content.classList.add("team__wrapper--active");*/
+        if (ev.target.nextElementSibling.classList.contains("team__wrapper--active")) {
+            ev.target.nextElementSibling.classList.remove("team__wrapper--active");
+        } else {
+            ev.target.nextElementSibling.classList.add("team__wrapper--active");
+        }
 
-        content.classList.add("team__wrapper--active");
-        //close sibling
         for (let i = 0; i < buttons.length; i++) {
             const e = buttons[i];
-            
-            if (e !== element) {   
-                const contentToHide = e.nextElementSibling;             
-                //contentToHide.style.maxHeight = "0";
-                
+
+            if (e !== element) {
+                const contentToHide = e.nextElementSibling;
+                //contentToHide.style.maxHeight = "0";                
                 //contentToHide.style.overflow = "hidden";
                 contentToHide.classList.remove("team__wrapper--active");
 
             }
-            
-        }    
-       
 
-        
+            
+
+        }
+
+
+
 
     });
 }
