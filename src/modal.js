e725f4886e253f23e0146631bd6cac721b971428
alex <60008473+alex83d@users.noreply.github.com>
2021-06-30 13:49:44 +0300

    form js

---
+++ src/modal.js
@@ -0,0 +1,75 @@
+const validateFields = (form, fieldArray) => {
+    fieldArray.forEach(field => {
+        field.removeClass("input-error");
+        if (field.val().trim() == "") {
+            field.addClass("input-error");
+        }
+    });
+
+    const inputError = form.find(".input-error");
+
+    return inputError.length == 0;
+}
+
+
+$('.form').submit((e) => {
+    e.preventDefault();
+
+    const form = $(e.currentTarget);
+    const name = form.find("[name = 'name']");
+    const phone = form.find("[name = 'phone']");
+    const comment = form.find("[name = 'comment']");
+    const to = form.find("[name = 'to']");
+    const modal = $('#modal');
+    const content = modal.find(".modal__content");
+
+    content.removeClass("error-modal");
+
+    const isValid = validateFields(form, [name, phone, comment, to])
+
+
+
+
+
+    if (isValid) {
+        const request = $.ajax({
+            type: "method",
+            url: "https://webdev-api.loftschool.com/sendmail",
+            method: "post",
+            data: {
+                name: name.val(),
+                phone: phone.val(),
+                comment: comment.val(),
+                to: to.val(),
+            },
+
+        });
+
+        request.done((data) => {
+            content.text(data.message)
+            //console.log(data);
+        });
+
+        request.fail((data) => {
+            const message = data.responseJSON.message;
+            content.text(message);
+            console.log(data);
+            content.addClass("error-modal");
+        });
+
+        request.always(() => {
+            $.fancybox.open({
+                src: "#modal",
+                type: "inline"
+            });
+        });
+    }
+});
+
+
+
+$('.app-submit-btn').click((e) => {
+    e.preventDefault();
+
+    $.fancybox.close();
+});
