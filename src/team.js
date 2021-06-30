2e1a0f9ad5f1d1aa6c546fca69525577a271158d
alex <60008473+alex83d@users.noreply.github.com>
2021-06-30 21:11:51 +0300

    team js arrow

--- src/team.js
+++ src/team.js
@@ -1,12 +1,15 @@
 const teamList = document.getElementById('#team__list');
 const buttons = document.querySelectorAll('.team__subtitle');
 const teamWrap = document.querySelectorAll('.team__wrapper');
+const arrow = document.querySelectorAll('.team-arrow')
+console.log(buttons);
 
 
 for (let index = 0; index < buttons.length; index++) {
     const element = buttons[index];
     element.addEventListener('click', ev => {
         ev.preventDefault();
+
         if (ev.target.nextElementSibling.classList.contains("team__wrapper--active")) {
             ev.target.nextElementSibling.classList.remove("team__wrapper--active");
         } else {
@@ -18,12 +21,28 @@ for (let index = 0; index < buttons.length; index++) {
 
             if (e !== element) {
                 const contentToHide = e.nextElementSibling;
-                //contentToHide.style.maxHeight = "0";                
-                //contentToHide.style.overflow = "hidden";
                 contentToHide.classList.remove("team__wrapper--active");
             }
 
+           
+
         }
+
+
+    });
+    //arrow up down
+    element.addEventListener("click",function(a) {
+        a.preventDefault();
+        if (a.target.firstElementChild.classList.contains("team-arrow--down")) {
+            a.target.firstElementChild.classList.remove("team-arrow--down");
+        } else {
+            a.target.firstElementChild.classList.add("team-arrow--down");
+        }
+
+        return console.log(a)
+            
+
 
     });
 }
+
