6388ebdf66db5c96e58e10036047f5fa4cabedec
alex <60008473+alex83d@users.noreply.github.com>
2021-07-10 00:31:08 +0300

    player ended

--- src/player.js
+++ src/player.js
@@ -2,10 +2,11 @@ let video;
 let durationControl;
 let soundControl;
 let intervalId;
+let soundLevel;
 
 
 const playBtn = document.querySelector(".player__btn-img");
-const soundBtn = document.querySelector(".sound__btn");
+const soundBtn = document.querySelector('#mic');
 const playerPlayBtn = document.querySelector(".duration__img");
 
 
@@ -27,6 +28,22 @@ video.addEventListener('loadeddata', () => {
     durationControl.max = video.duration;
     durationControl.addEventListener('input', setVideoTime);
 
+    
+    soundBtn.addEventListener('click', soundOff);
+
+
+    soundControl = document.getElementById('volumeLevel');
+    soundControl.addEventListener('input', changeSoundVolume);
+    soundControl.min = 0;
+    soundControl.max = 10;
+    soundControl.value = soundControl.max / 5;
+
+});
+
+video.addEventListener('ended', function () {
+    playBtn.classList.toggle("player__btn-img--active");
+    video.currentTime = 0;
+    playerPlayBtn.classList.remove('duration__img--active');
 });
 
 
@@ -59,3 +76,26 @@ const updateTime = () => {
         .style
         .background = `linear-gradient(90deg, #E01F3D 0%, #E01F3D ${percent}%, #333333 ${percent}% )`;
 };
+
+const soundOff = () => {
+    if(video.volume === 0) {
+        video.volume = soundLevel;
+        soundControl.value = soundLevel * 10;
+        soundBtn.classList.remove('active');
+    } else {
+        soundLevel = video.volume;
+        video.volume = 0;
+        soundControl.value = 0;
+        soundBtn.classList.add('active');
+    }
+};
+
+const changeSoundVolume = () => {
+    video.volume = soundControl.value / 10;
+
+    if (video.volume === 0) {
+        soundBtn.classList.add('active');
+    } else {
+        soundBtn.classList.remove('active');
+    }
+}
